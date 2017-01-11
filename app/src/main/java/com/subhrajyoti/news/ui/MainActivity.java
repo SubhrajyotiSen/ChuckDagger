e2971469e4665b7e890ba4e65298bd16196e14cd
Subhrajyoti Sen <subhrajyoti12@gmail.com>
2017-01-11 16:32:58 +0530

    feat: add FAB to get more jokes

--- app/src/main/java/com/subhrajyoti/news/ui/MainActivity.java
+++ app/src/main/java/com/subhrajyoti/news/ui/MainActivity.java
@@ -1,13 +1,21 @@
 package com.subhrajyoti.news.ui;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
-import com.subhrajyoti.news.model.JokeModel;
 import com.subhrajyoti.news.MyApplication;
 import com.subhrajyoti.news.R;
+import com.subhrajyoti.news.model.JokeModel;
 import com.subhrajyoti.news.retrofit.RestAPI;
 
 import javax.inject.Inject;
@@ -21,7 +29,12 @@ public class MainActivity extends AppCompatActivity {
 
     @Inject
     Retrofit retrofit;
-    TextView textView;
+    private TextView textView;
+    private ProgressBar progressBar;
+    private FloatingActionButton floatingActionButton;
+    private Toolbar toolbar;
+    private Call<JokeModel> joke;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,12 +43,62 @@ public class MainActivity extends AppCompatActivity {
         ((MyApplication) getApplication()).getNetComponent().inject(this);
 
         textView = (TextView) findViewById(R.id.textView);
-        Call<JokeModel> joke = retrofit.create(RestAPI.class).getJoke();
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        floatingActionButton = (FloatingActionButton) findViewById(R.id.fab);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                newJoke();
+            }
+        });
 
+        newJoke();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.settings:
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle(getString(R.string.app_name));
+                builder.setMessage(R.string.app_description);
+                builder.setPositiveButton(getString(R.string.dialog_positive),
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                            }
+                        });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+                break;
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void newJoke(){
+        joke = retrofit.create(RestAPI.class).getJoke();
+        textView.setVisibility(View.INVISIBLE);
+        progressBar.setVisibility(View.VISIBLE);
         joke.enqueue(new Callback<JokeModel>() {
             @Override
             public void onResponse(Call<JokeModel> call, Response<JokeModel> response) {
                 String joke = response.body().getJoke();
+                progressBar.setVisibility(View.INVISIBLE);
+                textView.setVisibility(View.VISIBLE);
                 textView.setText(joke.replaceAll("&quot;", "\""));
                 Log.d("TAG",response.body().getJoke());
 
@@ -46,5 +109,6 @@ public class MainActivity extends AppCompatActivity {
                 textView.setText(t.toString());
             }
         });
+
     }
 }
